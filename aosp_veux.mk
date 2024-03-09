dd1d3ed61a975b64037dd6b3bdf9a6a6945e071b
aswinop <itsaswinop@gmail.com>
2024-03-09 16:43:44 +0530

    veux: pos bringup
    Signed-off-by: aswinop <itsaswinop@gmail.com>

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -13,17 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
 # Inherit some common Matrixx stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Matrixx 
-MATRIXX_BUILD_TYPE := Official
-MATRIXX_MAINTAINER := AswinOP
-MATRIXX_CHIPSET := SM6375
-MATRIXX_BATTERY := 5000mAh
-MATRIXX_DISPLAY := 1080X2400
-
-# Gapps
-WITH_GMS := true
+$(call inherit-product, aosp/lineage/config/common_full_phone.mk)
 
 
 # AOSP Recovery
@@ -31,12 +21,6 @@ TARGET_USES_AOSP_RECOVERY := true
 
 # BOOT_ANIMATION
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Nuke AudioFX
-TARGET_EXCLUDES_AUDIOFX := true
-
-#Blur effect
-TARGET_ENABLE_BLUR := true	
 
 # Device identifier
 PRODUCT_NAME := lineage_veux
