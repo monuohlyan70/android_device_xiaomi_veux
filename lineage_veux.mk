26d1cb0a99ae6315fdc5819620a6ab96c2389885
monuohlyan70 <155902159+monuohlyan70@users.noreply.github.com>
2024-03-10 11:47:16 +0000

    Revert "veux: pos bringup"
    This reverts commit dd1d3ed61a975b64037dd6b3bdf9a6a6945e071b.

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -13,7 +13,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
 # Inherit some common Matrixx stuff.
-$(call inherit-product, aosp/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Matrixx 
+MATRIXX_BUILD_TYPE := Official
+MATRIXX_MAINTAINER := AswinOP
+MATRIXX_CHIPSET := SM6375
+MATRIXX_BATTERY := 5000mAh
+MATRIXX_DISPLAY := 1080X2400
+
+# Gapps
+WITH_GMS := true
 
 
 # AOSP Recovery
@@ -21,6 +31,12 @@ TARGET_USES_AOSP_RECOVERY := true
 
 # BOOT_ANIMATION
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Nuke AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
+#Blur effect
+TARGET_ENABLE_BLUR := true	
 
 # Device identifier
 PRODUCT_NAME := lineage_veux
